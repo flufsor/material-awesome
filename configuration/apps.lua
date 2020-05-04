656984dfac59d9f99b633c12c99e4e35fc3cb3b4
flufsor <flufsor@gmail.com>
2020-05-04 16:49:23 +0200

    Changed default programs

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -8,21 +8,21 @@ local rofi_command = 'env /usr/bin/rofi -dpi ' .. get_dpi() .. ' -width ' .. wit
 return {
   -- List of apps to start by default on some actions
   default = {
-    terminal = 'konsole',
+    terminal = 'alacritty',
     rofi = rofi_command,
+    filemanager = 'pcmanfm',
     lock = 'i3lock-fancy',
-    quake = 'konsole',
     screenshot = '~/.config/awesome/configuration/utils/screenshot -m',
     region_screenshot = '~/.config/awesome/configuration/utils/screenshot -r',
     delayed_screenshot = 'sleep 10 ; ~/.config/awesome/configuration/utils/screenshot -r',
     
     -- Editing these also edits the default program
     -- associated with each tag/workspace
-    browser = 'env vivaldi',
+    browser = 'env firefox-developer-edition',
     editor = 'code', -- gui text editor
     social = 'env discord',
     game = rofi_command,
-    files = 'nautilus',
+    files = 'pcmanfm',
     music = rofi_command
   },
   -- List of apps to start once on start-up
@@ -31,10 +31,10 @@ return {
     'nm-applet --indicator', -- wifi
     --'blueberry-tray', -- Bluetooth tray icon
     --'xfce4-power-manager', -- Power manager
-    'ibus-daemon --xim --daemonize', -- Ibus daemon for keyboard
-    'scream -u -p 4011 -i virbr1', -- scream audio sink
-    'numlockx on', -- enable numlock
-    '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    --'ibus-daemon --xim --daemonize', -- Ibus daemon for keyboard
+    --'scream -u -p 4011 -i virbr1', -- scream audio sink
+    --'numlockx on', -- enable numlock
+    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     --'/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
 
     -- Add applications that need to be killed between reloads
